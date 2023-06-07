3a818131b09e0726b47cfb4afe08095f515b734a
Jose Toledo <jtoledo2019@inf.udec.cl>
2023-06-07 14:04:22 -0400

    se organizo las carpetas de todo el proyecto

--- src/ComponentesGlobales/Home.js
+++ src/ComponentesGlobales/Home.js
@@ -1,11 +1,9 @@
+
 import React from 'react';
-import { Container, Row, Col, Button, Badge, Image, Stack } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { Container, Row, Col, Stack , Button } from 'react-bootstrap';
+import { BsFillBugFill, BsArrowDown } from 'react-icons/bs';
 import { motion } from 'framer-motion';
-
-import Imagen from '../Imagenes/home.png';
-
-import '../EstilosGlovales/Home.css';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -23,64 +21,76 @@ const Home = () => {
   };
 
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.3 }}
-    >
-      <Container className='d-flex flex-column justify-content-center align-items-center home'>
-        <Row className='flex-column justify-content-center align-items-center'>
-          <Col md='auto' className='mt-5 mb-5'>
-            <motion.h1
-              className='text-center mb-5'
-              initial={{ opacity: 0, y: -50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.3 }}
-            >
-              <Badge bg='dark'>BugFixer</Badge>
-            </motion.h1>
-            <motion.p
-              className='parrafo'
-              initial={{ opacity: 0, y: 50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.5 }}
-            >
-              Bienvenido a <strong>BugFixer</strong>, la plataforma que facilita la comunicación entre usuarios y
-              administradores para reportar, gestionar y solucionar problemas en el software. Nuestro objetivo es mejorar
-              la calidad y eficiencia en la detección y corrección de errores, permitiendo un proceso de desarrollo de
-              software más ágil y efectivo.
-            </motion.p>
-          </Col>
+    <Container fluid style={{ height: '100vh' }}>
+      <Row style={{ height: '100%' }}>
+        <Col md={8} className='d-flex flex-column columna111'>
+          <Row style={{ flex: '10%' }}>
+            <Col className='barraSuperiorXD'>
+              <Stack direction='horizontal' gap={5}>
+                <div className='Logo'>
+                  <BsFillBugFill className='bugIcon' />
+                  <span className='bugTextXD'>BugFixer</span>
+                </div>
+                <div className='textossS'>Para equipos</div>
+                <div className='textossS'>Para personas</div>
+              </Stack>
+            </Col>
+          </Row>
+          <Row style={{ flex: '55%' }}>
+            <Col className='headerColo'>
+              <motion.h1
+                className='headerTexto'
+                initial={{ rotate: 0 }}
+                animate={{ rotate: 360 }}
+                transition={{ duration: 0.6, repeat: Infinity, repeatType: 'loop', repeatDelay: 5 }}
+              >
+                Soluciona tus errores junto a BugFixer
+              </motion.h1>
+            </Col>
+          </Row>
+          <Row style={{ flex: '20%' }}>
+            <Col className='textColo'>
+              <p className='parrafooo'>
+                BugFixer es la solución perfecta para tus proyectos. Colabora con clientes, administradores y
+                depuradores de forma ágil y sencilla. Resuelve errores y fallos de manera rápida y eficiente. Optimiza
+                tu proceso de desarrollo y garantiza la calidad de tu proyecto
+              </p>
+            </Col>
+          </Row>
+          <Row style={{ flex: '15%' }}>
+            <Col className='contenedor-iconooo'>
+              <BsArrowDown className='iconooo' />
+            </Col>
+          </Row>
+        </Col>
 
-          <Col md='auto' xs={12} className='prubea mb-5'>
-            <Stack direction='horizontal' gap={5}>
-              <Button variant='danger' className='botones-home' onClick={handleDebButtonClick}>
+        <Col md={4} className='columna222'>
+          <Stack direction='horizontal' gap={5}>
+              <Button variant='outline-primary' className='botones-home' onClick={handleDebButtonClick}>
                 Depurador
               </Button>
-              <Button variant='primary' className='botones-home' onClick={handleAdminButtonClick}>
+              <Button variant='outline-danger' className='botones-home' onClick={handleAdminButtonClick}>
                 Administrador
               </Button>
-              <Button variant='success' className='botones-home' onClick={handleUserButtonClick}>
+              <Button variant='outline-success' className='botones-home' onClick={handleUserButtonClick}>
                 Usuario
               </Button>
             </Stack>
-          </Col>
-
-          <Col md='auto' xs={12} className='mt-3'>
-            <motion.div
-              className='imagen-container'
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ duration: 0.5, delay: 0.7 }}
-            >
-              <Image src={Imagen} alt='Imagen-Home' className='imagen' />
-            </motion.div>
-          </Col>
-        </Row>
-      </Container>
-    </motion.div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
 export default Home;
+
+
+
+
+
+
+
+
+
+
+
